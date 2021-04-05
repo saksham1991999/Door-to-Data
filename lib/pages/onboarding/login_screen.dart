96c3d1c64c7d1cff125e18d57b730d39620a44b0
Saksham Mittal <181210045@nitdelhi.ac.in>
2021-04-06 00:09:11 +0530

    initial UI

---
+++ lib/pages/onboarding/login_screen.dart
@@ -0,0 +1,83 @@
+import 'package:doortodata/pages/onboarding/signup_screen.dart';
+import 'package:doortodata/widgets/onboarding/already_have_an_account_acheck.dart';
+import 'package:doortodata/widgets/onboarding/rounded_button.dart';
+import 'package:doortodata/widgets/onboarding/rounded_input_field.dart';
+import 'package:doortodata/widgets/onboarding/rounded_password_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: size.height,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/main_top.png",
+                width: size.width * 0.35,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Image.asset(
+                "assets/images/login_bottom.png",
+                width: size.width * 0.4,
+              ),
+            ),
+        SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "LOGIN",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: size.height * 0.03),
+              SvgPicture.asset(
+                "assets/icons/login.svg",
+                height: size.height * 0.35,
+              ),
+              SizedBox(height: size.height * 0.03),
+              RoundedInputField(
+                hintText: "Your Email",
+                onChanged: (value) {},
+              ),
+              RoundedPasswordField(
+                onChanged: (value) {},
+              ),
+              RoundedButton(
+                text: "LOGIN",
+                press: () {},
+              ),
+              SizedBox(height: size.height * 0.03),
+              AlreadyHaveAnAccountCheck(
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return SignUpScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+          ],
+        ),
+      )
+    );
+  }
+}
---
+++ lib/pages/onboarding/welcome_screen.dart
@@ -0,0 +1,85 @@
+import 'package:doortodata/constants.dart';
+import 'package:doortodata/pages/onboarding/login_screen.dart';
+import 'package:doortodata/pages/onboarding/signup_screen.dart';
+import 'package:doortodata/widgets/onboarding/rounded_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body:Container(
+        height: size.height,
+        width: double.infinity,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/main_top.png",
+                width: size.width * 0.3,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/main_bottom.png",
+                width: size.width * 0.2,
+              ),
+            ),
+        SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "WELCOME TO DOOR TO DATA",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: size.height * 0.05),
+              SvgPicture.asset(
+                "assets/icons/chat.svg",
+                height: size.height * 0.45,
+              ),
+              SizedBox(height: size.height * 0.05),
+              RoundedButton(
+                text: "LOGIN",
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return LoginScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+              RoundedButton(
+                text: "SIGN UP",
+                color: DoorToDataAppTheme.nearlyBlue,
+                textColor: Colors.black,
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return SignUpScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+          ],
+        ),
+      )
+    );
+  }
+}
